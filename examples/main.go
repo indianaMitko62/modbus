779c481dd0d845dc139db718d1f4e9c83474e320
indianaMitko62 <indiana.mitko@gmail.com>
2024-10-30 17:07:47 +0200

    created tag

--- examples/main.go
+++ examples/main.go
@@ -30,7 +30,7 @@ func main() {
 
 	client := modbus.NewClient(handler)
 
-	vendorName, productCode, majorMinorVersion, err := client.ReadDeviceIdentificationSpecific(1)
+	vendorName, productCode, majorMinorVersion, err := client.ReadDeviceIdentificationBasic()
 	if err != nil {
 		fmt.Println(err)
 		return
