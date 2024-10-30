26ea18826401b74769331690e67f97cd8c9aabb4
indianaMitko62 <indiana.mitko@gmail.com>
2024-10-30 15:35:33 +0200

    Added example usage of client.ReadDeviceIdentificationBasic

---
+++ examples/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"actshad.dev/modbus"
+)
+
+const (
+	rtuDevice = "/dev/ttyUSB0"
+)
+
+func main() {
+	handler := modbus.NewRTUClientHandler(rtuDevice)
+	handler.BaudRate = 115200
+	handler.DataBits = 8
+	handler.Parity = "N"
+	handler.StopBits = 1
+	handler.SlaveId = 2
+	handler.Logger = log.New(os.Stdout, "rtu: ", log.LstdFlags)
+	err := handler.Connect()
+	if err != nil {
+		fmt.Println("error connecting to serial")
+		return
+	}
+	defer handler.Close()
+
+	client := modbus.NewClient(handler)
+
+	vendorName, productCode, majorMinorVersion, err := client.ReadDeviceIdentificationBasic(0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("vendor name: %s\n", vendorName)
+	fmt.Printf("product code: %s\n", productCode)
+	fmt.Printf("version: %s\n", majorMinorVersion)
+
+	for i := 0; i < 5; i++ {
+		results, err := client.ReadHoldingRegisters(8, 2)
+		if err != nil || results == nil {
+			fmt.Println(err.Error())
+			return
+		}
+		for i := 0; i < len(results); i++ {
+			fmt.Println(results[i])
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
